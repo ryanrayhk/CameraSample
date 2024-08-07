8ca5636c5c4b7e0cb7bca9048443f055c4f067ad
RayChow19 <ryanrayhk@gmail.com>
2024-08-07 22:21:51 +0800

    Initial Commit

---
+++ CameraSample/CameraSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraSampleApp.swift
+//  CameraSample
+//
+//  Created by Ray Chow on 7/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct CameraSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
