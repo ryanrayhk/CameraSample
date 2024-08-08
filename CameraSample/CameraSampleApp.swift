491f325600c7dd8d2bd13f0ce6f3b037c9cd1ed1
RayChow19 <ryanrayhk@gmail.com>
2024-08-09 00:18:43 +0800

    init code from apple, link in description
    https://developer.apple.com/tutorials/sample-apps/capturingphotos-camerapreview

--- CameraSample/CameraSampleApp.swift
+++ CameraSample/CameraSampleApp.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 @main
 struct CameraSampleApp: App {
+    init() {
+        UINavigationBar.applyCustomAppearance()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CameraView()
         }
+    }
+}
+
+fileprivate extension UINavigationBar {
+    
+    static func applyCustomAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
 }
